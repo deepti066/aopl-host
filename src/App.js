3599b9b65dd9280b17861281a94c297150e4e0a0
saranga pani sarma <sarangapani@siqes.com>
2024-11-13 17:14:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     });
   }, []);
 
-  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+  useDocTitle("Aryahi Optinet | ISP | System Integrations");
 
   return (
     <>
