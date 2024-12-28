c7c39359a0bf94199becad6df732ab87f43940b8
Deepti066 <deeptigupta5638@gmail.com>
2024-12-28 18:09:16 +0530

    updated intro page

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Intro = () => {
     return (
         <>
-            <div className="m-auto max-w-6xl p-2 md:p-12 h-5/6 bg-gradient-to-br from-gray-800 via-blue-900 to-black" id="about">
+            <div className="m-auto ml-0 mr-0 mt-0 mb-0 p-2 md:p-12 h-5/6 bg-gradient-to-br from-gray-800 via-blue-900 to-black" id="about">
 
                 <div className="flex flex-col-reverse lg:flex-row py-8 justify-between lg:text-left" data-aos="fade-up">
                     <div className="lg:w-1/2 flex flex-col lg:mx-4 justify-center">
@@ -17,26 +17,29 @@ const Intro = () => {
                             Fast, Secure, and Scalable Cloud Hosting for Your Business
                         </h3>
                         <div>
-                            <p className="my-3 text-xl text-gray-300 font-semibold">
+                            <p className="my-3 text-xl text-white font-semibold">
                                 Experience lightning-fast performance, unparalleled uptime, and robust security with our cutting-edge cloud hosting solutions. 
                             </p>
                         </div>
 
                         <div>
-                            <p className="my-3 text-xl text-gray-300 font-semibold">
+                            <p className="my-3 text-xl text-white font-semibold">
                                 Whether you're running a small business or managing an enterprise, our cloud hosting services are designed to scale with your needs.
                             </p>
                         </div>
 
                         <div>
-                            <p className="my-3 text-xl text-gray-300 font-semibold">
+                            <p className="my-3 text-xl text-white font-semibold">
                                 Enjoy 24/7 support, automated backups, and an intuitive control panel to manage your hosting effortlessly.
                             </p>
                         </div>
 
-                        <Link to="/contact" className="text-white bg-blue-700 hover:bg-blue-600 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
+                        <Link to="/contact" className="text-white bg-indigo-500 hover:bg-indigo-600 inline-flex items-center justify-center 
+                        w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
                             Contact us
-                            <svg className="w-4 h-4 ml-1 group-hover:translate-x-2 transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                            <svg className="w-4 h-4 ml-1 group-hover:translate-x-2 transition-transform duration-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" 
+                            clipRule="evenodd"></path></svg>
                         </Link>
                     </div>
                 </div>
